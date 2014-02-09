100079723a6c3ab27f03f0616d0a9484514fb72b
Nicolas McCurdy <thenickperson@gmail.com>
2014-02-09 03:24:29 -0500

    Lower socket.io's log level to 2

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get('/users', user.list);
 var server = http.createServer(app);
 
 var io = require('socket.io').listen(server);
+io.set('log level', 2);
 io.sockets.on('connection', function (socket) {
   socket.on('message sent', function (message) {
     io.sockets.emit('message recieved', message);
